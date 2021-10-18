d7f1a6a5366fa4057b38300d8e3a1b52a2756a2b
AlanBuechner <47373257+AlanBuechner@users.noreply.github.com>
2021-10-18 13:53:25 -0600

    updated gitignore

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/yaml-cpp
-# Build directory: C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/yaml-cpp/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("util")
--- out/build/x64-Debug/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/yaml-cpp/util
-# Build directory: C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/yaml-cpp/out/build/x64-Debug/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
